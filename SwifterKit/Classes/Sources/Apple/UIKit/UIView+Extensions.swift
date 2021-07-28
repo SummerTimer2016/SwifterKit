07addc0f44083bb8e901c74b09005ef034a3dded
jianglu <jianglu@99elink.com>
2021-07-28 19:21:24 +0800

    添加自定义方法

--- SwifterKit/Classes/Sources/Apple/UIKit/UIView+Extensions.swift
+++ SwifterKit/Classes/Sources/Apple/UIKit/UIView+Extensions.swift
@@ -399,6 +399,26 @@ public extension UIView {
             subview.removeFromSuperview()
         }
     }
+    
+    /**
+     Returns the UIViewController object that manages the receiver.
+     */
+    func viewController() -> UIViewController? {
+        
+        var nextResponder: UIResponder? = self
+        
+        repeat {
+            nextResponder = nextResponder?.next
+            
+            if let viewController = nextResponder as? UIViewController {
+                return viewController
+            }
+            
+        } while nextResponder != nil
+        
+        return nil
+    }
+    
 }
 
 // MARK: - UIView animatable extension
